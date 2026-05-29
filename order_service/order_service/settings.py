a839a7b93b90b3454bd6b47bddf7db70f9f3e6c9
didin2003 <didinpg8080@gmail.com>
2026-05-29 12:59:58 +0530

    Fix environment variable name for CORS settings

--- order_service/order_service/settings.py
+++ order_service/order_service/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL") == "True"
+CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 

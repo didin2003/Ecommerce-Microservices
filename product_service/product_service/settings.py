a9d1158cd6634f40775dbd85848f5992606ef306
didin2003 <didinpg8080@gmail.com>
2026-06-04 14:39:15 +0530

    Add CSRF_TRUSTED_ORIGINS to settings

--- product_service/product_service/settings.py
+++ product_service/product_service/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 # Application definition
 
 INSTALLED_APPS = [
